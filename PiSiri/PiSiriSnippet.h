926b25963bf875a81404c214941824e44353ec5a
Kepler Sticka-Jones <kbsj269@gmail.com>
2012-02-09 18:13:05 -0700

    Removed more unneeded code

--- PiSiri/PiSiriSnippet.h
+++ PiSiri/PiSiriSnippet.h
@@ -15,11 +15,6 @@
 // there can be 0, 1 or more different SESnippet classes per AE Extension
 // each different SESnippet class must be "registered" in *Extenion's -[initWithSystem:system]
 
-#error iOSOpenDev Post-Template Project Creation Requirement (remove these lines after completed) -- \
- Add a Copy Bundle Resources Build Phase to compile PiSiriNib.xib: \
- (1) go to TARGETS > Build Phases > Add Build Phase > Add Copy Bundle Resources, drag and drop PiSiriNib.xib into the Copy Bundle Resources item list \
- (2) drag and drop the Copy Bundle Resources Build Phase so that it is ABOVE the Run Script Build Phase
-
 @interface PiSiriSnippet : NSObject<SESnippet> {
     UIView* _view;
 	IBOutlet UIView* _nib;
